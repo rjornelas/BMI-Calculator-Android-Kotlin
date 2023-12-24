202635ba32a9171de13260a999488a90d763c216
rjornelas <rafael.j.ornelas@gmail.com>
2023-12-24 14:41:13 -0300

    Implementation of BMI calculation and updating values ​​in View NumberPicker with setOnValueChangedList

--- BMICalculatorSample2/app/src/main/java/com/example/bmi_calculator_sample2/MainActivity.kt
+++ BMICalculatorSample2/app/src/main/java/com/example/bmi_calculator_sample2/MainActivity.kt
@@ -1,11 +1,65 @@
 package com.example.bmi_calculator_sample2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bmi_calculator_sample2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.weightPicker.minValue = 30
+        binding.weightPicker.maxValue = 300
+
+        binding.heightPicker.minValue = 100
+        binding.heightPicker.maxValue = 250
+
+        binding.weightPicker.setOnValueChangedListener{_,_,_ ->
+            calculateBMI()
+        }
+
+        binding.heightPicker.setOnValueChangedListener{_,_,_ ->
+            calculateBMI()
+        }
+
+    }
+
+    private fun calculateBMI() {
+        val height = binding.heightPicker.value
+        val doubleHeight = height.toDouble()/100
+
+        val weight = binding.weightPicker.value
+
+        val bmi = weight.toDouble() / (doubleHeight * doubleHeight)
+
+        binding.tvResult.text = String.format("Your BMI is: %2f", bmi)
+
+        binding.tvHealthy.text = healthyMessage(bmi)
+    }
+
+    private fun healthyMessage(bmi: Double): String {
+        return when {
+            bmi < 18.5 -> {
+                showHealthMessage("Underweight")
+            }
+            bmi < 25.0 -> {
+                showHealthMessage("Health")
+            }
+            bmi < 30.0 -> {
+                showHealthMessage("Overweight")
+            }
+            else -> {
+                showHealthMessage("obese")
+            }
+        }
+    }
+
+    private fun showHealthMessage(healthyMessage: String): String {
+        return String.format("Considered: %s", healthyMessage)
     }
 }
