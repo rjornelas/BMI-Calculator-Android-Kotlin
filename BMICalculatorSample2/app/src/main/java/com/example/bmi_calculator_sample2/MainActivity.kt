987fe9fd8b8b0fe63691cc64715728e81fe6a836
rjornelas <rafael.j.ornelas@gmail.com>
2023-12-24 14:52:01 -0300

    Implementation of color change logic according to BMI results

--- BMICalculatorSample2/app/src/main/java/com/example/bmi_calculator_sample2/MainActivity.kt
+++ BMICalculatorSample2/app/src/main/java/com/example/bmi_calculator_sample2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.bmi_calculator_sample2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.bmi_calculator_sample2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -39,27 +40,39 @@ class MainActivity : AppCompatActivity() {
 
         binding.tvResult.text = String.format("Your BMI is: %2f", bmi)
 
-        binding.tvHealthy.text = healthyMessage(bmi)
+        healthyMessage(bmi)
     }
 
-    private fun healthyMessage(bmi: Double): String {
-        return when {
+    private fun healthyMessage(bmi: Double) {
+
+        var messageText = ""
+        var color = 0
+
+        when {
             bmi < 18.5 -> {
-                showHealthMessage("Underweight")
+                messageText = showHealthMessage("Underweight")
+                color = R.color.under_weight
             }
             bmi < 25.0 -> {
-                showHealthMessage("Health")
+                messageText = showHealthMessage("Normal weight")
+                color = R.color.normal_weight
             }
             bmi < 30.0 -> {
-                showHealthMessage("Overweight")
+                messageText = showHealthMessage("Overweight")
+                color = R.color.over_weight
             }
             else -> {
-                showHealthMessage("obese")
+                messageText = showHealthMessage("obese")
+                color = R.color.obese
             }
         }
+
+        binding.tvHealthy.text = messageText
+        binding.tvHealthy.setTextColor(ContextCompat.getColor(this, color))
     }
 
     private fun showHealthMessage(healthyMessage: String): String {
+
         return String.format("Considered: %s", healthyMessage)
     }
 }
